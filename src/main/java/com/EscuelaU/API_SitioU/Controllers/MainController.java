518c3aa22a403a1984d0f8072fcd00b9ea76b3f9
Manuel N. Fabri <manuel.nicolas.fabri@accenture.com>
2019-05-22 15:25:48 -0300

    Conexion a BD y agregado de @RestController

--- src/main/java/com/EscuelaU/API_SitioU/Controllers/MainController.java
+++ src/main/java/com/EscuelaU/API_SitioU/Controllers/MainController.java
@@ -3,17 +3,20 @@ package com.EscuelaU.API_SitioU.Controllers;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.EscuelaU.API_SitioU.Models.Greeting;
 
+@RestController
 
 public class MainController {
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hola, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping(value = "/saludar", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Escuela!") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
