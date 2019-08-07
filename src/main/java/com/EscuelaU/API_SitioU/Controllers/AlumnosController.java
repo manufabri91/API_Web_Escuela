ac15263fa0333bda0207c5746748c7edf91f3aed
Manuel Fabri <manuelnfabri@yahoo.com.ar>
2019-08-07 00:22:08 -0300

    creo controlador y modelo de alumnos

--- src/main/java/com/EscuelaU/API_SitioU/Controllers/AlumnosController.java
+++ src/main/java/com/EscuelaU/API_SitioU/Controllers/AlumnosController.java
@@ -1,24 +1,38 @@
 package com.EscuelaU.API_SitioU.Controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.EscuelaU.API_SitioU.Models.Greeting;
+import com.EscuelaU.API_SitioU.Models.Alumno;
+import com.EscuelaU.API_SitioU.Models.AlumnoDAO;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/alumnos")
 public class AlumnosController {
-    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private AlumnoDAO alumnoDAO;
+
     
-    @RequestMapping(value = "/lista", method = RequestMethod.GET)
-    public Greeting greeting(@RequestParam(value="name", defaultValue="alumno 1") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format("alumno: ", name));
+    @RequestMapping(value = "/listarAlumnos", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Alumno>>  listarAlumnos() {
+    	Iterable<Alumno> alumnos = alumnoDAO.findAll();
+		return new ResponseEntity<Iterable<Alumno>>(alumnos, HttpStatus.OK);
     }
+    
+    @RequestMapping(value = "/cargarAlumno", method = RequestMethod.POST)
+    public ResponseEntity<Alumno> cargarAlumno(@Valid @RequestBody Alumno alumno) {
+        Alumno alumnoCreado = alumnoDAO.save(alumno);
+        return new ResponseEntity<Alumno>(alumnoCreado, HttpStatus.CREATED);
+    }
+    
 }
---
+++ src/main/java/com/EscuelaU/API_SitioU/Models/AlumnoDAO.java
@@ -0,0 +1,7 @@
+package com.EscuelaU.API_SitioU.Models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlumnoDAO extends CrudRepository<Alumno, Long> {
+
+}
