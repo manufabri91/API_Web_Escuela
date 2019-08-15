efc54f50512732fa9f6cf89fbb088d56fc11aa60
Manuel Fabri <manuelnfabri@yahoo.com.ar>
2019-08-14 22:19:21 -0300

    update controller

--- src/main/java/com/EscuelaU/API_SitioU/Controllers/AlumnosController.java
+++ src/main/java/com/EscuelaU/API_SitioU/Controllers/AlumnosController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,16 +26,35 @@ public class AlumnosController {
     private AlumnoDAO alumnoDAO;
 
     
-    @RequestMapping(value = "/listarAlumnos", method = RequestMethod.GET)
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
     public ResponseEntity<Iterable<Alumno>>  listarAlumnos() {
     	Iterable<Alumno> alumnos = alumnoDAO.findAll();
 		return new ResponseEntity<Iterable<Alumno>>(alumnos, HttpStatus.OK);
     }
-    
-    @RequestMapping(value = "/cargarAlumno", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/cargar", method = RequestMethod.POST)
     public ResponseEntity<Alumno> cargarAlumno(@Valid @RequestBody Alumno alumno) {
         Alumno alumnoCreado = alumnoDAO.save(alumno);
         return new ResponseEntity<Alumno>(alumnoCreado, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/borrar/{legajo}")
+    public ResponseEntity<Alumno> borrar(@PathVariable Long legajo) {
+    	if(alumnoDAO.findById(legajo).isPresent()) {
+    		alumnoDAO.deleteById(legajo);
+    		return new ResponseEntity<>(HttpStatus.OK);
+    	} 
+    	return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(value = "/actualizar")
+    public ResponseEntity<Alumno> actualiar(@Valid @RequestBody Alumno alumno) {
+    	
+    	if(alumnoDAO.findById(alumno.getLegajo()).isPresent()) {
+    		alumnoDAO.save(alumno);
+    		return new ResponseEntity<>(HttpStatus.OK);
+    	} 
+    	return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     
 }
