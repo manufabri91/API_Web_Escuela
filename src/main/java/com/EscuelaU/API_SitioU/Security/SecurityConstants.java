f60e8a430ab7974d5d52dddcbc8bf62ecf4de370
Manuel Fabri <manuelnfabri@yahoo.com.ar>
2019-08-17 12:36:06 -0300

    cambio dia

--- src/main/java/com/EscuelaU/API_SitioU/Security/SecurityConstants.java
+++ src/main/java/com/EscuelaU/API_SitioU/Security/SecurityConstants.java
@@ -2,8 +2,8 @@ package com.EscuelaU.API_SitioU.Security;
 
 public class SecurityConstants {
 	public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final long EXPIRATION_TIME = 86_400_000; // 1 dia
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/registrarse";
-
+}
